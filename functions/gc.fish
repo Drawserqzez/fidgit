16ace2b7d919a382d8bc0687d95323fb9ad184d0
Drawserqzez <me@holton.lol>
2023-05-26 14:11:44 +0200

    Fix missing argument error

--- functions/gc.fish
+++ functions/gc.fish
@@ -1,7 +1,7 @@
 function gc -d "Shortcut for git commit"
     set -l commit_message $argv[1]
 
-    if test ! -z $commit_message
+    if test ! -z "$commit_message"
         git commit -m $commit_message
     else
         git commit
